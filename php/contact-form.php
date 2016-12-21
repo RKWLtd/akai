666ee7c96815dfd44b2d26434c7ed229cf55f086
Daniel Mountford <daniel.mountford@rkw.co>
2016-12-21 15:33:25 +0000

    Contact page edit

--- php/contact-form.php
+++ php/contact-form.php
@@ -42,7 +42,7 @@ if(get_magic_quotes_gpc()) {
 // Enter the email address that you want to emails to be sent to.
 
 //$address = "example@example.com";
-$address = "enquiries@akaionline.co";
+$address = "daniel.mountford@rkw.co";
 
 
 // Configuration option.
